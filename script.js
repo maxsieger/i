ccaadb4929b630a9135267691f114fbe47a94b48
maxsieger <maksnov10@mail.ru>
2024-04-07 13:00:28 +0300

    fixed bag, THANK YOU ASPIR

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let ru = document.querySelector(".portfolio-img-1");
 let eng = document.querySelector(".portfolio-img-2");
 
-ru.addEventListener("click", ru_open())
-eng.addEventListener("click", eng_open())
+ru.addEventListener("click", ru_open)
 
-function ru_open(){
-    let ru_div = document.querySelector("div-portfolio__img-1");
+function ru_open() {
+    let ru_div = document.querySelector(".div-portfolio__img-1");
     ru.classList.add("portfolio-img-1--activ");
     ru_div.classList.add("div-portfolio__img-1--activ");
+}
+
+let burger = document.querySelector(".hamburger");
+let menu = document.querySelector(".nav-blok__navbar-2");
+
+burger.addEventListener("click", burger_open)
+
+function burger_open() {
+    menu.classList.toggle("nav-blok__navbar-2--open");
+    let name = menu.className;
+    console.log(name)
 }
