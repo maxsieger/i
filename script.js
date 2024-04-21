a8f01a749ec058a220cbbb04ddcf26578104a26f
maxsieger <maksnov10@mail.ru>
2024-04-21 09:11:59 +0300

    process

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let ru = document.querySelector(".portfolio-img-1");
-let eng = document.querySelector(".portfolio-img-2");
-
-ru.addEventListener("click", ru_open)
-
-function ru_open() {
-    let ru_div = document.querySelector(".div-portfolio__img-1");
-    ru.classList.add("portfolio-img-1--activ");
-    ru_div.classList.add("div-portfolio__img-1--activ");
-}
-
 let burger = document.querySelector(".hamburger");
 let menu = document.querySelector(".nav-blok__navbar-2");
 
@@ -18,4 +7,14 @@ function burger_open() {
     menu.classList.toggle("nav-blok__navbar-2--open");
     let name = menu.className;
     console.log(name)
+}
+
+function openModal() {
+    var modal = document.getElementsByClassName("div-portfolio__img-1--off");
+    modal.style.display = "flex";
+}
+
+    function closeModal() {
+    var modal = document.getElementById("div-portfolio__img-1--off");
+    modal.style.display = "none";
 }
